f3ac607ee6862a5c9a20af16fec2a1f6f3c3c927
dgalhardi <diegowsu@gmail.com>
2021-01-24 17:38:13 -0300

    Adding some simple projecs, learning some concepts about android.

---
+++ SimpleLoginPage/app/src/main/java/github/io/dgalhadi/desafio1_loginpage/MainActivity.java
@@ -0,0 +1,14 @@
+package github.io.dgalhadi.desafio1_loginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
